2a650df579da88a1ade2b86fc75427e63e280ae1
switch023 <yuuhamada23@gmail.com>
2020-06-10 22:13:29 +0900

    create_auto-update_function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-      `<div class="message__box">
+      `<div class="message__box" data-message-id=${message.id}>
         <div class="message__box__list">
           <div class="message__box__list__user">
             ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
      return html;
    } else {
      var html =
-      `<div class="message__box">
+      `<div class="message__box" data-message-id=${message.id}>
         <div class="message__box__list">
           <div class="message__box__list__user">
             ${message.user_name}
@@ -63,4 +63,29 @@ $(function(){
       $('.form__submit').prop('disabled', false);
     })
   })
+  var reloadMessages = function() {
+    var last_message_id = $('.message__box:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'GET',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
